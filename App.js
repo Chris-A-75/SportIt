31e697f0ed9b5de5b0226f8602856ecf2fae4183
Chris A <chrisaccaoui@pprotonmail.com>
2024-09-14 13:50:58 +0300

    Added a Home, Reservations and Profile tabs at the bottom

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
+//these are packages used for stuff in the app (mostly UI stuff)
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import  MaterialIcons  from 'react-native-vector-icons/MaterialIcons';
+
+//these are references to the different pages we'll have
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ReservationsScreen from './screens/ReservationsScreen';
+
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Profile') {
+            iconName = 'person';
+          } else if (route.name === 'Reservations') {
+            iconName = 'event';
+          }
+
+          return <MaterialIcons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: styles.tabBar.activeTintColor,
+        tabBarInactiveTintColor: styles.tabBar.inactiveTintColor,
+        tabBarStyle: styles.tabBar.style,
+        tabBarLabelStyle: styles.tabBar.labelStyle,
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Reservations" component={ReservationsScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
 
@@ -16,5 +58,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tabBar: {
+    activeTintColor: 'tomato',
+    inactiveTintColor: 'gray',
+    style: {
+      backgroundColor: '#fff', // background color of the tab bar
+      height: 60, // height of the tab bar
+      borderTopWidth: 0, // remove top border
+      paddingBottom: 5, // padding at the bottom
+    },
+    labelStyle: {
+      fontSize: 12,
+    },
   },
 });
